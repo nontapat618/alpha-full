3c5fedf8617a5ba185fb6e7bc2e284f1be0e7827
Test Alpha <nontapat.618@gmail.com>
2017-10-20 07:53:24 +0000

    edit and show article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,39 @@
 class ArticlesController < ApplicationController
     
+    def index
+       @articles = Article.all 
+    end
+    
     def new
         @article  = Article.new
     end
     
     def create
-        render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
+        if @article.save
+            flash[:notice] = "Article was save succesfully"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+    
+    def edit
+       @article = Article.find(params[:id]) 
+    end
+    
+    def update
+       @article = Article.find(params[:id])
+       if @article.update(article_params)
+            flash[:notice] = "Article was edit succesfully"
+            redirect_to article_path(@article)           
+       else
+           render 'edit'
+       end
+    end
+    
+    def show
+       @article = Article.find(params[:id]) 
     end
     
     private
