bf57761e295570877b4e55d50d11617b71f85a50
Test Alpha <nontapat.618@gmail.com>
2017-11-03 19:22:39 +0000

    add edit user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,24 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:sucess] = "Welcome to the alpha blog #{@user.username}"
+            flash[:success] = "Welcome to the alpha blog #{@user.username}"
             redirect_to articles_path
         else
             render 'new' 
+        end
+    end
+    
+    def edit
+        @user = User.find(params[:id])    
+    end
+    
+    def update
+        @user = User.find(params[:id]) 
+        if @user.update(user_params)
+            flash[:success] = "Edit Account Successfully"
+            redirect_to articles_path
+        else
+            render 'edit' 
         end
     end
     
