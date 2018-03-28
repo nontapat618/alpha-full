4f4bda630ccd00d31072ff9d86cecb7afda48332
Test Alpha <nontapat.618@gmail.com>
2018-03-28 17:07:16 +0000

    add permission to category create page

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -6,6 +6,7 @@ class CategoriesControllerTest < ActionController::TestCase
         @category = Category.new
         @category.name = "sports"
         @category.save
+        @user = User.create(username: "John",email: "John@example.com",password: "password",admin: true)
     end
     
     test "should get categories index" do
@@ -14,6 +15,7 @@ class CategoriesControllerTest < ActionController::TestCase
     end
 
     test "should get new" do
+        session[:user_id] = @user.id
         get :new
         assert_response :success
     end
@@ -22,5 +24,11 @@ class CategoriesControllerTest < ActionController::TestCase
         get(:show, {'id' => @category.id})
         assert_response :success
     end
-    
+   
+   test "should not be created if user is not admin" do
+        assert_no_difference 'Category.count' do
+            post :create, category: {name: "sport"} 
+        end
+        assert_redirected_to categories_path   
+   end
 end
